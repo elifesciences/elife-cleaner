cc09f1ee7e64a5d271a914f57a36e7efb944aa45
Graham Nott <gnott@starglobal.ca>
2021-03-30 10:58:08 -0700

    Change README.rst to README.md

--- README.rst
+++
@@ -1,9 +0,0 @@
-elife-cleaner
-=============================
-
-Clean and transform article submission files into a consistent format
-
-License
-=========
-
-`The MIT License <http://opensource.org/licenses/mit-license.php>`_
--- setup.py
+++ setup.py
@@ -2,13 +2,14 @@ from setuptools import setup
 
 import elifecleaner
 
-with open('README.rst') as fp:
+with open("README.md") as fp:
     readme = fp.read()
 
 setup(name='elifecleaner',
     version=elifecleaner.__version__,
     description='Clean and transform article submission files into a consistent format.',
     long_description=readme,
+    long_description_content_type="text/markdown",
     packages=['elifecleaner'],
     license = 'MIT',
     install_requires=[
