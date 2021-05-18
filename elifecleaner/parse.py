e35c4ccdab5bf7b6696ff67da600b643b59e0285
Graham Nott <gnott@starglobal.ca>
2021-05-17 19:08:26 -0700

    Stub check_ejp_zip() in parse module.

--- elifecleaner/parse.py
+++ elifecleaner/parse.py
@@ -1,2 +1,7 @@
-def dummy():
+from elifecleaner import zip_lib
+
+
+def check_ejp_zip(zip_file, tmp_dir):
+    asset_file_name_map = zip_lib.unzip_zip(zip_file, tmp_dir)
+    # todo!!!
     return True
--- tests/test_parse.py
+++ tests/test_parse.py
@@ -1,10 +1,16 @@
+import os
 import unittest
 from elifecleaner import parse
+from tests.helpers import delete_files_in_folder
 
 
 class TestParse(unittest.TestCase):
     def setUp(self):
-        pass
+        self.temp_dir = "tests/tmp"
 
-    def test_dummy(self):
-        self.assertTrue(parse.dummy())
+    def tearDown(self):
+        delete_files_in_folder(self.temp_dir, filter_out=[".keepme"])
+
+    def test_check_ejp_zip(self):
+        zip_file = "tests/test_data/30-01-2019-RA-eLife-45644.zip"
+        self.assertTrue(parse.check_ejp_zip(zip_file, self.temp_dir))
