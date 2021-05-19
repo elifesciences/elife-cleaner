a660ee2008c2fae987c418fdd5d88c3737ff8ad0
Graham Nott <gnott@starglobal.ca>
2021-05-18 17:40:55 -0700

    Find the article XML file / manifest file.

--- elifecleaner/parse.py
+++ elifecleaner/parse.py
@@ -1,3 +1,4 @@
+import re
 from elifecleaner import zip_lib
 
 
@@ -5,3 +6,17 @@ def check_ejp_zip(zip_file, tmp_dir):
     asset_file_name_map = zip_lib.unzip_zip(zip_file, tmp_dir)
     # todo!!!
     return True
+
+
+def article_xml_asset(asset_file_name_map):
+    """
+    find the article XML file name,
+    e.g. 30-01-2019-RA-eLife-45644/30-01-2019-RA-eLife-45644.xml
+    """
+    xml_asset = None
+    match_pattern = re.compile(r"^(.*)/\1.xml$")
+    for asset in asset_file_name_map.items():
+        if re.match(match_pattern, asset[0]):
+            xml_asset = asset
+            break
+    return xml_asset
--- tests/test_parse.py
+++ tests/test_parse.py
@@ -1,6 +1,6 @@
 import os
 import unittest
-from elifecleaner import parse
+from elifecleaner import parse, zip_lib
 from tests.helpers import delete_files_in_folder
 
 
@@ -14,3 +14,13 @@ class TestParse(unittest.TestCase):
     def test_check_ejp_zip(self):
         zip_file = "tests/test_data/30-01-2019-RA-eLife-45644.zip"
         self.assertTrue(parse.check_ejp_zip(zip_file, self.temp_dir))
+
+    def test_article_xml_name(self):
+        zip_file = "tests/test_data/30-01-2019-RA-eLife-45644.zip"
+        asset_file_name_map = zip_lib.unzip_zip(zip_file, self.temp_dir)
+        expected = (
+            "30-01-2019-RA-eLife-45644/30-01-2019-RA-eLife-45644.xml",
+            "tests/tmp/30-01-2019-RA-eLife-45644/30-01-2019-RA-eLife-45644.xml",
+        )
+        xml_asset = parse.article_xml_asset(asset_file_name_map)
+        self.assertEqual(xml_asset, expected)
