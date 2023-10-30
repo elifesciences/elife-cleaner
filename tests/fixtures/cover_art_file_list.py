74d4666e08bebd4e6aab7fd7e96fbdb12446b620
Graham Nott <gnott@starglobal.ca>
2023-10-30 14:47:24 -0700

    Add cover art functions to transform.py

---
+++ tests/fixtures/cover_art_file_list.py
@@ -0,0 +1,23 @@
+from collections import OrderedDict
+
+
+EXPECTED = [
+    OrderedDict(
+        [
+            ("file_type", "cover_art"),
+            ("id", "1119364"),
+            ("upload_file_nm", "Potential striking image.tif"),
+            (
+                "custom_meta",
+                [
+                    OrderedDict(
+                        [
+                            ("meta_name", "Title"),
+                            ("meta_value", "Potential Striking Image"),
+                        ]
+                    )
+                ],
+            ),
+        ]
+    )
+]
